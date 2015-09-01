e7e3dd155d8b9dda0540db655331e2d79302a7d1
Ernesto Rojo <gamedev@ernestorojo.com>
2015-09-01 19:39:52 -0400

    Added a button to MainMenu to load the MainGame scene

---
+++ Game/Assets/Scripts/ChangeLevel.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeLevel : MonoBehaviour
+{
+    public void LoadLevel(int level)
+    {
+        Application.LoadLevelAsync(level);
+    }
+
+    public void LoadLevel(string level)
+    {
+        Application.LoadLevelAsync(level);
+    }
+}
