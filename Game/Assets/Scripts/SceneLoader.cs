3876ce1433cee99859fe3e04a025bbd5b07ff760
Ernesto Rojo <gamedev@ernestorojo.com>
2015-12-24 00:28:05 -0500

    Ok, so levels load now. The game is no where near as playable on a device as it is on my laptop. :(

--- Game/Assets/Scripts/SceneLoader.cs
+++ Game/Assets/Scripts/SceneLoader.cs
@@ -3,15 +3,15 @@ using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 {
-    public Object[] _scenes = null;
+    public string[] _scenes = null;
 
     // Use this for initialization
     void Start()
     {
         foreach (var scene in _scenes)
         {
-            Debug.Log("Loading Scene: " + scene.name);
-            SceneManager.LoadSceneAsync(scene.name, LoadSceneMode.Additive);
+            Debug.Log("Loading Scene: " + scene);
+            SceneManager.LoadSceneAsync(scene, LoadSceneMode.Additive);
         }
 
         Destroy(gameObject);
